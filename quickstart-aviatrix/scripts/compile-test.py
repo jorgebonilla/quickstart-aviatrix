0a2789a1fed66951b176936eb6d014e249d366e4
jorgebonilla <jorge.luis.bonilla@gmail.com>
2018-01-15 16:33:53 -0800

    Fix

--- quickstart-aviatrix/scripts/compile-test.py
+++ quickstart-aviatrix/scripts/compile-test.py
@@ -1,4 +1,4 @@
 import py_compile
-py_compile.compile('../scripts/aviatrix_poller.py')
-py_compile.compile('../scripts/aviatrix_controller.py')
-py_compile.compile('../scripts/aviatrix_gateway.py')
+py_compile.compile('../quickstart-aviatrix/scripts/aviatrix_poller.py')
+py_compile.compile('../quickstart-aviatrix/scripts/aviatrix_controller.py')
+py_compile.compile('../quickstart-aviatrix/scripts/aviatrix_gateway.py')
